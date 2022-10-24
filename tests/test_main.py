84c7decc77ec1ecfbd5d4ecd9da7bad1c3afa5e7
mahojo99 <mejohan@hiof.no>
2022-10-24 16:20:41 +0200

    Changed first test back to True

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,7 @@
 from main import isLeapYear
 
 def test_year_divided_by_four_and_not_one_hundred_is_not_zero():
-    assert isLeapYear(2104) == False
+    assert isLeapYear(2104) == True
 
 def test_year_divided_by_four_hundred_is_zero():
     assert isLeapYear(2104) == True
