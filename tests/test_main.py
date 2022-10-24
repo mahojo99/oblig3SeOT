38756835b417d0ff8a3572619d52b77dfa69147c
mahojo99 <mejohan@hiof.no>
2022-10-24 16:03:40 +0200

    Initial commit

---
+++ tests/test_main.py
@@ -0,0 +1,14 @@
+from main import isLeapYear
+
+def test_year_divided_by_four_and_not_one_hundred_is_not_zero():
+    assert isLeapYear(2104) == True
+
+def test_year_divided_by_four_hundred_is_zero():
+    assert isLeapYear(2104) == True
+
+def test_year_divided_by_four_and_not_one_hundred_is_zero():
+    assert isLeapYear(2105) == False
+
+def test_year_divided_by_four_hundred_is_not_zero():
+    assert isLeapYear(2105) == False
+
